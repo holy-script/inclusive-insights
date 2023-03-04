2fc07eee213fb1a1b7847acc5df5a626ff936fc2
Palash Johri <palashjohricool@gmail.com>
2023-03-04 20:30:17 +0530

    quasar project initialized, vercel config added, boilerplate removed

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="text-h2">Hello, world!</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
